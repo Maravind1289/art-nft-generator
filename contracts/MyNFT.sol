cb6cce2ac6aef0c08cd5e628cfb6f317eb0de02b
MAREDDY ARAVIND REDDY <mareeddyaravind986609@gmail.com>
2024-08-28 10:00:27 +0530

    Initial commit

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyNFT {
+    string public name = "MyNFT";
+    string public symbol = "MNFT";
+    
+    // Add your contract code here
+}
+
